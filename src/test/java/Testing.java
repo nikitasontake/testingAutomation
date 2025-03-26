3f50a554bdb473a4ebd828c8b0c8843b10256215
nikitasontake <101354370+nikitasontake@users.noreply.github.com>
2025-03-26 18:31:29 +0530

    added

---
+++ src/test/java/Testing.java
@@ -0,0 +1,11 @@
+public class Testing {
+    public static void main(String [] args){
+        Testing t1 = new Testing();
+        t1.testing();
+    }
+
+    public void testing(){
+        System.out.println("Welcome to IntelliJ ----");
+    }
+
+}
